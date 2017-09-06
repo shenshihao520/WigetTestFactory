ddd11a124b77d60c246bc773a224b696cf8bc4be
shenshihao520 <794262212@qq.com>
2017-09-06 11:11:49 +0800

    桌面小程序

---
+++ app/src/main/java/com/example/shenshihao520/wigettestfactory/widget/little_widget/MyWidgetProvider.java
@@ -0,0 +1,11 @@
+package com.example.shenshihao520.wigettestfactory.widget.little_widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by shenshihao520 on 2017/8/25.
+ */
+
+public class MyWidgetProvider extends AppWidgetProvider {
+
+}
